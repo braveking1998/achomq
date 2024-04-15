5c359f2246a936eadaab0e343362eb4831145fd0
Braveking <bravekingah@gmail.com>
2024-04-15 17:29:36 +0330

    A small bug

--- database/migrations/2024_02_19_140908_add_type_column_to_multi_games_table.php
+++ database/migrations/2024_02_19_140908_add_type_column_to_multi_games_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('multi_games', function (Blueprint $table) {
-            $table->foreignIdFor('\App\Models\MultiGameType')->after('game_id')->nullable()->constrained('multi_game_types');
+            $table->foreignIdFor('\App\Models\MultiGameType')->after('id')->nullable()->constrained('multi_game_types');
         });
     }
 
