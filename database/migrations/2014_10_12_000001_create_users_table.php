e46b7baa2e1a0c92d9c77c74e6850a5dd31acddc
Braveking <bravekingah@gmail.com>
2024-05-12 12:34:21 +0330

    version 0.0.13

--- database/migrations/2014_10_12_000001_create_users_table.php
+++ database/migrations/2014_10_12_000001_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('phone_number');
+            $table->string('phone_number')->unique();
             $table->string('password');
             $table->integer('points')->default(0);
             $table->integer('coins')->default(200);
