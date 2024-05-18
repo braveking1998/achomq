8558ddeb1ef6f728e5d1047babcf82553d17e06e
Braveking <bravekingah@gmail.com>
2024-05-18 08:39:58 +0330

    add show component to qeustion mangement section

---
+++ resources/js/Pages/Admin/Questions/Show.vue
@@ -0,0 +1,67 @@
+<template>
+  <Head title="مشاهده سوال" />
+  <auth-without-sidebar-layout>
+    <template #header>
+      <app-breadcrumbs :breadcrumbs="breadcrumbs">
+        <template #right-side>
+          <div class="flex gap-4">
+            <Link
+              :href="route('questions.edit', question.id)"
+              class="btn-primary bg-green-500"
+              >ویرایش سوال</Link
+            >
+            <Link
+              :href="route('questions.destroy', question.id)"
+              class="btn-primary bg-red-500"
+              method="delete"
+              as="button"
+              >حذف سوال</Link
+            >
+          </div>
+        </template>
+      </app-breadcrumbs>
+    </template>
+
+    <template #content>
+      <app-box class="flex flex-col gap-4 p-6">
+        <p class="text-gray-500"><span>متن سوال: </span>{{ question.text }}</p>
+        <p class="text-gray-500">
+          <span>دسته بندی: </span>{{ question.category.name }}
+        </p>
+        <p class="text-gray-500"><span>سطح: </span>{{ question.level.name }}</p>
+        <p class="text-gray-500">
+          <span>تاریخ ایجاد: </span>{{ formatedDate }}
+        </p>
+        <ul class="flex flex-col gap-4">
+          <li
+            v-for="(answer, index) in question.answers"
+            :key="answer.id"
+            class="text-gray-500"
+          >
+            <span>پاسخ {{ index + 1 }}: </span>{{ answer.text }}
+            <span v-if="answer.is_correct" class="pr-4">✔️</span>
+          </li>
+        </ul>
+      </app-box>
+    </template>
+  </auth-without-sidebar-layout>
+</template>
+
+<script setup>
+import AuthWithoutSidebarLayout from "@/Layouts/AuthWithoutSidebarLayout.vue";
+import AppBreadcrumbs from "@/Components/AppBreadcrumbs.vue";
+import AppBox from "@/Components/AppBox.vue";
+import { Head, Link } from "@inertiajs/vue3";
+import { useFormattedShamsiDate } from "@/Composables/date.js";
+
+const breadcrumbs = [
+  { label: "داشبورد", url: route("dashboard") },
+  { label: "سوالات", url: route("admin.questions.index") },
+];
+const props = defineProps({
+  question: Object,
+});
+
+// Date
+const { formatedDate } = useFormattedShamsiDate(props.question.created_at);
+</script>
