8e6f4fdf4d4bab769bdb52a333638a24e13fad1a
Braveking <bravekingah@gmail.com>
2024-05-20 12:22:42 +0330

    fix a bug related to the link of the profile images

--- app/Models/ProfileImage.php
+++ app/Models/ProfileImage.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ProfileImage extends Model
 {
@@ -16,10 +17,17 @@ class ProfileImage extends Model
         'user_id',
     ];
 
-
+    protected $appends = ['src'];
 
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    protected function src(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => asset('storage/' . $this->file_path),
+        );
     }
 }
